9509a15918f4b4541192e196528653f04600c2a0
markrissinger@gmail.com <markrissinger@gmail.com>
2025-01-11 10:42:13 -0500

    leetcode75 - mergealternately

---
+++ LeetCode/LeetCode75/MergeStringsAlternately/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LeetCode75.MergeStringsAlternately
+{
+    public class Solution
+    {
+        public string MergeAlternately(string word1, string word2)
+        {
+            StringBuilder result = new();
+
+            int w1Length = word1.Length;
+            int w2Length = word2.Length;
+
+            int i1 = 0;
+            int i2 = 0;
+
+            while (i1 < w1Length || i2 < w2Length)
+            {
+                if (i1 < w1Length)
+                {
+                    result.Append(word1[i1]);
+                }
+                if (i2 < w2Length)
+                {
+                    result.Append(word2[i2]);
+                }
+                i1++;
+                i2++;
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ TestProject/LeetCode75/MergeStringsAlternatively.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.LeetCode75.MergeStringsAlternately;
+
+namespace TestProject.LeetCode75
+{
+    public class MergeStringsAlternatively
+    {
+        [Fact]
+        public void Test()
+        {
+            Solution solution = new();
+            string result1 = solution.MergeAlternately("abc", "pqr");
+            string result2 = solution.MergeAlternately("ab", "pqrs");
+            string result3 = solution.MergeAlternately("abcd", "pq");
+            Assert.Equal("apbqcr", result1);
+            Assert.Equal("apbqrs", result2);
+            Assert.Equal("apbqcd", result3);
+        }
+    }
+}
