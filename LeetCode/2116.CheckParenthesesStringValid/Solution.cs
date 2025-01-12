e16f844686eb2c69042db5824b098ccc6cdb1c13
markrissinger@gmail.com <markrissinger@gmail.com>
2025-01-11 21:40:23 -0500

    check parentheses string valid

---
+++ LeetCode/2116.CheckParenthesesStringValid/Solution.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._2116.CheckParenthesesStringValid
+{
+    public class Solution
+    {
+        public bool CanBeValid(string s, string locked)
+        {
+            int sLength = s.Length;
+            if (int.IsOddInteger(sLength))
+            {
+                return false;
+            }
+
+            Stack<int> openBrackets = new();
+            Stack<int> unlocked = new();
+
+            for (int i = 0; i < sLength; i++)
+            {
+                if (locked[i] == '0')
+                {
+                    unlocked.Push(i);
+                }
+                else if (s[i] == '(')
+                {
+                    openBrackets.Push(i);
+                }
+                else
+                {
+                    if (openBrackets.Count > 0)
+                        openBrackets.Pop();
+                    else if (unlocked.Count > 0)
+                        unlocked.Pop();
+                    else
+                        return false;
+                }
+            }
+
+            while (openBrackets.Count > 0 && unlocked.Count > 0 && openBrackets.Peek() < unlocked.Peek())
+            {
+                openBrackets.Pop();
+                unlocked.Pop();
+            }
+
+            if (openBrackets.Count > 0)
+                return false;
+
+            return true;
+        }
+
+        //public bool CanBeValid(string s, string locked)
+        //{
+        //    int sLength = s.Length;
+        //    if (int.IsOddInteger(sLength))
+        //    {
+        //        return false;
+        //    }
+
+        //    int balance = 0;
+        //    int unlocked = 0;
+
+        //    for (int i = 0; i < s.Length; i++)
+        //    {
+
+        //        if (locked[i] == '1')
+        //        {
+        //            if (s[i] == '(')
+        //            {
+        //                balance++;
+        //            }
+        //            if (s[i] == ')')
+        //            {
+        //                balance--;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            unlocked++;
+        //        }
+
+        //        if (balance < 0)
+        //        {
+        //            if (unlocked == 0)
+        //            {
+        //                return false;
+        //            }
+        //            balance++;
+        //            unlocked--;
+        //        }
+        //    }
+
+        //    if (balance - unlocked > 0)
+        //    {
+        //        return false;
+        //    }
+
+        //    return true;
+        //}
+    }
+}
---
+++ TestProject/CheckParenthesesStringValid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode._2116.CheckParenthesesStringValid;
+
+namespace TestProject
+{
+    public class CheckParenthesesStringValid
+    {
+        [Fact]
+        public void Test()
+        {
+            Solution solution = new();
+            bool result1 = solution.CanBeValid(")", "0");
+            bool result2 = solution.CanBeValid("))()))", "010100");
+            bool result3 = solution.CanBeValid("())()))()(()(((())(()()))))((((()())(())", "1011101100010001001011000000110010100101");
+            bool result4 = solution.CanBeValid("())(()(()(())()())(())((())(()())((())))))(((((((())(()))))(", "100011110110011011010111100111011101111110000101001101001111");
+
+            Assert.False(result1);
+            Assert.True(result2);
+            Assert.True(result3);
+            Assert.False(result4);
+        }
+    }
+}
