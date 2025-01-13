30fb1dd359cca152d0bc173cfe05b2f2718a5330
markrissinger@gmail.com <markrissinger@gmail.com>
2025-01-12 19:38:08 -0500

    included daily test

--- TestProject/MinimumLength.cs
+++ TestProject/MinimumLength.cs
@@ -14,16 +14,11 @@ namespace TestProject
         {
             Solution solution = new();
 
-            int[] list1 = new int[2] { 1, 2 };
-            int[] list2 = new int[2] { 3, 4 };
-            int[][] lists = new int[][] { list1, list2 };
-            List<int> result1 = solution.ZigzagTraversal(new int[][] { new int[2] { 1, 2 }, new int[2] { 3, 4 } }).ToList();
-            List<int> result2 = solution.ZigzagTraversal(new int[][] { new int[2] { 2, 1 }, new int[2] { 2, 1 }, new int[2] { 2, 1 } }).ToList();
-            List<int> result3 = solution.ZigzagTraversal(new int[][] { new int[3] { 1, 2, 3 }, new int[3] { 4, 5, 6 }, new int[3] { 7, 8, 9 } }).ToList();
+            int result1 = solution.MinimumLength("abaacbcbb");
+            int result2 = solution.MinimumLength("aa");
 
-            Assert.Equal(new List<int>() { 1, 4 }, result1);
-            Assert.Equal(new List<int>() { 2, 1, 2 }, result2);
-            Assert.Equal(new List<int>() { 1, 3, 5, 7, 9 }, result3);
+            Assert.Equal( 5, result1);
+            Assert.Equal(2, result2);
         }
     }
 }
