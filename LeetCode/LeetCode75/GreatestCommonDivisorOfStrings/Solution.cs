e0fa1b687be079e6027582d0688dda1ea4f71511
markrissinger@gmail.com <markrissinger@gmail.com>
2025-01-13 21:19:56 -0500

    gcd of strings

---
+++ LeetCode/LeetCode75/GreatestCommonDivisorOfStrings/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LeetCode75.GreatestCommonDivisorOfStrings
+{
+    public class Solution
+    {
+        public string GcdOfStrings(string str1, string str2)
+        {
+            if (str1 + str2 != str2 + str1)
+            {
+                return string.Empty;
+            }
+
+            return str1.Substring(0, Gcd(str1.Length, str2.Length));
+        }
+
+        private int Gcd(int a, int b)
+        {
+            return b == 0 ? a : Gcd(b, a % b);
+        }
+    }
+}
---
+++ TestProject/LeetCode75/GreatestCommonDivisorOfStrings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.LeetCode75.GreatestCommonDivisorOfStrings;
+
+namespace TestProject.LeetCode75
+{
+    public class GreatestCommonDivisorOfStrings
+    {
+        [Fact]
+        public void Test()
+        {
+            Solution solution = new();
+            string result1 = solution.GcdOfStrings("ABCABC", "ABC");
+            string result2 = solution.GcdOfStrings("ABABAB", "ABAB");
+            string result3 = solution.GcdOfStrings("LEET", "CODE");
+            Assert.Equal("ABC", result1);
+            Assert.Equal("AB", result2);
+            Assert.Equal("", result3);
+        }
+    }
+}
