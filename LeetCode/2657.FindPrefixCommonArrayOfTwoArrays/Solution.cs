9272a7492ad56dc52fa5070cfc5492b9260c20d7
markrissinger@gmail.com <markrissinger@gmail.com>
2025-01-13 20:42:19 -0500

    daily problem

---
+++ LeetCode/2657.FindPrefixCommonArrayOfTwoArrays/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._2657.FindPrefixCommonArrayOfTwoArrays
+{
+    public class Solution
+    {
+        public int[] FindThePrefixCommonArray(int[] A, int[] B)
+        {
+            int[] result = new int[A.Length];
+
+            int[] lookup = new int[A.Length];
+
+            int currCounter = 0;
+
+            for(int i = 0; i < A.Length; i++)
+            {
+                int AIndex = A[i] - 1;
+                int BIndex = B[i] - 1;
+                lookup[AIndex]++;
+                lookup[BIndex]++;
+
+                if (AIndex == BIndex)
+                {
+                    currCounter++;
+                }
+                else
+                {
+                    if (lookup[AIndex] == 2)
+                    {
+                        currCounter++;
+                    }
+                    if (lookup[BIndex] == 2)
+                    {
+                        currCounter++;
+                    }
+                }                
+
+                result[i] = currCounter;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TestProject/FindThePrefixCommonArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode._2657.FindPrefixCommonArrayOfTwoArrays;
+
+namespace TestProject
+{
+    public class FindThePrefixCommonArray
+    {
+        [Fact]
+        public void Test()
+        {
+            Solution solution = new();
+
+            int[] result1 = solution.FindThePrefixCommonArray(new int[] { 1, 3, 2, 4 }, new int[] { 3, 1, 2, 4 });
+            int[] result2 = solution.FindThePrefixCommonArray(new int[] { 2, 3, 1 }, new int[] { 3, 1, 2 });
+            Assert.Equal(new int[] { 0, 2, 3, 4 }, result1);
+            Assert.Equal(new int[] { 0, 1, 3 }, result2);
+        }
+    }
+}
