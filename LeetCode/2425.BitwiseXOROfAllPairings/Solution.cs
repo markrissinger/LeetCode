ebe8147e56f0ff1612eeece0b07a92bd76d66fb9
markrissinger@gmail.com <markrissinger@gmail.com>
2025-01-16 10:24:21 -0500

    daily challenge - xor all numbers

---
+++ LeetCode/2425.BitwiseXOROfAllPairings/Solution.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._2425.BitwiseXOROfAllPairings
+{
+    public class Solution
+    {
+        public int XorAllNums(int[] nums1, int[] nums2)
+        {
+            int num1Length = nums1.Length;
+            int num2Length = nums2.Length;
+
+            Dictionary<int, int> frequency = [];
+
+            for (int i = 0; i < num1Length; i++)
+            {
+                int currentNum = nums1[i];
+                frequency.TryGetValue(currentNum, out var currentCount);
+                frequency[currentNum] = currentCount + num2Length;
+            }
+
+            for (int i = 0; i < num2Length; i++)
+            {
+                int currentNum = nums2[i];
+                frequency.TryGetValue(currentNum, out var currentCount);
+                frequency[currentNum] = currentCount + num1Length;
+            }
+
+            int result = 0;
+            foreach (var (key, value) in frequency)
+            {
+                if (value % 2 == 1)
+                {
+                    result ^= key;
+                }
+            }
+
+            return result;
+        }
+
+
+        public int XorAllNums2(int[] nums1, int[] nums2)
+        {
+            int xor1 = 0;
+            int xor2 = 0;
+
+            int num1Length = nums1.Length;
+            int num2Length = nums2.Length;
+
+            if (num2Length % 2 != 0)
+            {
+                for (int i = 0; i < num1Length; i++)
+                {
+                    xor1 ^= nums1[i];
+                }
+            }
+
+            if (num1Length % 2 != 0)
+            {
+                for (int i = 0; i < num2Length; i++)
+                {
+                    xor2 ^= nums2[i];
+                }
+            }
+
+            return xor1 ^ xor2;
+        }
+    }
+}
---
+++ TestProject/BitwiseXOROfAllPairings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode._2425.BitwiseXOROfAllPairings;
+
+namespace TestProject
+{
+    public class BitwiseXOROfAllPairings
+    {
+        [Fact]
+        public void Test()
+        {
+            Solution solution = new();
+
+            int result1 = solution.XorAllNums(new int[] { 2, 1, 3 }, new int[] { 10, 2, 5, 0 });
+            int result2 = solution.XorAllNums(new int[] { 1, 2 }, new int[] { 3, 4 });
+
+            Assert.Equal(13, result1);
+            Assert.Equal(0, result2);
+
+            result1 = solution.XorAllNums2(new int[] { 2, 1, 3 }, new int[] { 10, 2, 5, 0 });
+            result2 = solution.XorAllNums2(new int[] { 1, 2 }, new int[] { 3, 4 });
+
+            Assert.Equal(13, result1);
+            Assert.Equal(0, result2);
+        }
+    }
+}
