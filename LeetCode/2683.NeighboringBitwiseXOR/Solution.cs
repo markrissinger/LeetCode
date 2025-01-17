389412fef39a4a4654684ae53dc47e332a1f8a0d
markrissinger@gmail.com <markrissinger@gmail.com>
2025-01-16 19:39:47 -0500

    daily - neighboring bitwise xor

---
+++ LeetCode/2683.NeighboringBitwiseXOR/Solution.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._2683.NeighboringBitwiseXOR
+{
+    public class Solution
+    {
+        public bool DoesValidArrayExist(int[] derived)
+        {
+            int arrayLength = derived.Length;
+
+            int[] original = new int[arrayLength + 1];
+
+            // 0 starter
+            original[0] = 0;
+            for (int i = 0; i < arrayLength; i++)
+            {
+                original[i + 1] = derived[i] ^ original[i];
+            }
+
+            bool check0 = (original[0] == original[original.Length - 1]);
+            if (check0)
+                return true;
+
+            // 1 starter
+            original[0] = 1;
+            for (int i = 0; i < arrayLength; i++)
+            {
+                original[i + 1] = derived[i] ^ original[i];
+            }
+
+            bool check1 = (original[0] == original[original.Length - 1]);
+            if (check1)
+                return true;
+
+            return false;
+        }
+
+
+        public bool DoesValidArrayExist2(int[] derived)
+        {
+            int XOR = 0;
+
+            // elements up until the last
+            for (int i = 0; i < derived.Length; i++)
+            {
+                XOR = XOR ^ derived[i];
+            }
+
+            return XOR == 0;
+        }
+
+
+        public bool DoesValidArrayExist3(int[] derived)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < derived.Length; i++)
+            {
+                sum += derived[i];
+            }
+
+            return sum % 2 == 0;
+        }
+    }
+}
---
+++ TestProject/NeighboringBitwiseXOR.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode._2683.NeighboringBitwiseXOR;
+
+namespace TestProject
+{
+    public class NeighboringBitwiseXOR
+    {
+        [Fact]
+        public void Test()
+        {
+            Solution solution = new();
+
+            bool result1 = solution.DoesValidArrayExist([1,1,0]);
+            bool result2 = solution.DoesValidArrayExist([1, 1]);
+            bool result3 = solution.DoesValidArrayExist([1, 0]);
+
+            Assert.True(result1);
+            Assert.True(result2);
+            Assert.False(result3);
+
+            result1 = solution.DoesValidArrayExist2([1, 1, 0]);
+            result2 = solution.DoesValidArrayExist2([1, 1]);
+            result3 = solution.DoesValidArrayExist2([1, 0]);
+
+            Assert.True(result1);
+            Assert.True(result2);
+            Assert.False(result3);
+
+            result1 = solution.DoesValidArrayExist3([1, 1, 0]);
+            result2 = solution.DoesValidArrayExist3([1, 1]);
+            result3 = solution.DoesValidArrayExist3([1, 0]);
+
+            Assert.True(result1);
+            Assert.True(result2);
+            Assert.False(result3);
+        }
+    }
+}
