7c8a1df1bc3eff4c8b6514b7524ed9ec206848ed
markrissinger@gmail.com <markrissinger@gmail.com>
2025-01-21 14:12:14 -0500

    daily leetcode - gridgame

---
+++ LeetCode/2017.GridGame/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._2017.GridGame
+{
+    public class Solution
+    {
+        public long GridGame(int[][] grid)
+        {
+            int n = grid[0].Length;
+
+            long firstRowSum = 0;
+            for (int num = 0; num < n; num++)
+            {
+                firstRowSum += grid[0][num];
+            }
+
+            long secondRowSum = 0;
+            long minimumSum = long.MaxValue;
+            for (int num = 0; num < n; ++num)
+            {
+                firstRowSum -= grid[0][num];
+
+                minimumSum = Math.Min(minimumSum, Math.Max(firstRowSum, secondRowSum));
+
+                secondRowSum += grid[1][num];
+            }
+
+            return minimumSum;
+        }
+    }
+}
---
+++ TestProject/GridGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode._2017.GridGame;
+
+namespace TestProject
+{
+    public class GridGame
+    {
+        [Fact]
+        public void Test()
+        {
+            Solution solution = new();
+
+            long result1 = solution.GridGame([[2, 5, 4], [1, 5, 1]]);
+            long result2 = solution.GridGame([[3, 3, 1], [8, 5, 2]]);
+            long result3 = solution.GridGame([[1, 3, 1, 15], [1, 3, 3, 1]]);
+
+            Assert.Equal(4, result1);
+            Assert.Equal(4, result2);
+            Assert.Equal(7, result3);
+        }
+    }
+}
