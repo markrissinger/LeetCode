e5caa469bf22160d16472ce89844f7b25d970c74
markrissinger@gmail.com <markrissinger@gmail.com>
2025-01-22 19:15:08 -0500

    daily leetcode - count servers that communicate

---
+++ LeetCode/1267.CountServersThatCommunicate/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._1267.CountServersThatCommunicate
+{
+    public class Solution
+    {
+        public int CountServers(int[][] grid)
+        {
+            int m = grid.Length;
+            int n = grid[0].Length;
+
+            int[] rowCounts = new int[m];
+            int[] colCounts = new int[n];
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        rowCounts[i]++;
+                        colCounts[j]++;
+                    }
+                }
+            }
+
+            int result = 0;
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (grid[i][j] == 1 && 
+                        (rowCounts[i] > 1 || colCounts[j] > 1))
+                    {
+                        result++;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TestProject/CountServers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode._1267.CountServersThatCommunicate;
+
+namespace TestProject
+{
+    public class CountServers
+    {
+        [Fact]
+        public void Test()
+        {
+            Solution solution = new();
+
+            int result1 = solution.CountServers([[1, 0], [0, 1]]);
+            int result2 = solution.CountServers([[1, 0], [1, 1]]);
+            int result3 = solution.CountServers([[1, 1, 0, 0], [0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 0, 1]]);
+
+            Assert.Equal(0, result1);
+            Assert.Equal(3, result2);
+            Assert.Equal(4, result3);
+        }
+    }
+}
