563b7ed94ce532a6a79e0dd753825fe60333e566
markrissinger@gmail.com <markrissinger@gmail.com>
2025-01-23 19:35:05 -0500

    daily leetcode - find eventual safe states

---
+++ LeetCode/802.FindEventualSafeStates/Solution.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._802.FindEventualSafeStates
+{
+    public class Solution
+    {
+        public IList<int> EventualSafeNodes(int[][] graph)
+        {
+            int n = graph.Length;
+            int[] indegree = new int[n];      // stores edges entering node x
+            List<List<int>> adjacent = new(); // stores nodes coming from x
+            for (int i = 0; i < n; i++)
+            {
+                adjacent.Add([]);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                foreach(int node in graph[i])
+                {
+                    adjacent[node].Add(i);
+                    indegree[i]++;
+                }
+            }
+
+            Queue<int> queue = new(); // BFS leaf --> parent nodes
+            for (int i = 0; i < n; i++)
+            {
+                if (indegree[i] == 0)
+                {
+                    queue.Enqueue(i);
+                }
+            }
+
+            bool[] safe = new bool[n]; // true if node is safe
+            while (queue.Count > 0)
+            {
+                int node = queue.Dequeue();
+                safe[node] = true;
+
+                foreach (int neighbor in adjacent[node])
+                {
+                    indegree[neighbor]--;
+                    if (indegree[neighbor] == 0)
+                    {
+                        queue.Enqueue(neighbor);
+                    }
+                }
+            }
+
+            List<int> result = new();
+            for (int i = 0; i < n; i++)
+            {
+                if (safe[i])
+                {
+                    result.Add(i);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TestProject/EventualSafeNodes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode._802.FindEventualSafeStates;
+
+namespace TestProject
+{
+    public class EventualSafeNodes
+    {
+        [Fact]
+        public void Test()
+        {
+            Solution solution = new();
+
+            List<int> result1 = solution.EventualSafeNodes([[1, 2], [2, 3], [5], [0], [5], [], []]).ToList();
+            List<int> result2 = solution.EventualSafeNodes([[1, 2, 3, 4], [1, 2], [3, 4], [0, 4], []]).ToList();
+
+            Assert.Equal([2, 4, 5, 6], result1);
+            Assert.Equal([4], result2);
+        }
+    }
+}
