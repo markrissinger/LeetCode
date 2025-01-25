8058a83f8261c1523dac55671f7cd7d9e83f5883
markrissinger@gmail.com <markrissinger@gmail.com>
2025-01-24 20:05:07 -0500

    daily leetcode - lexicographically smallest array

---
+++ LeetCode/2948.MakeLexicographicallySmallestArrayBySwappingElements/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._2948.MakeLexicographicallySmallestArrayBySwappingElements
+{
+    public class Solution
+    {
+        public int[] LexicographicallySmallestArray(int[] nums, int limit)
+        {
+            List<int> numsSorted = new List<int> (nums).Order().ToList();
+
+            int currentGroup = 0;
+            Dictionary<int, int> numberToGroup = new() { { numsSorted[0], currentGroup } };
+            Dictionary<int, LinkedList<int>> groupToList = new() { { currentGroup, new LinkedList<int>(new List<int>() { numsSorted[0] }) } };
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (Math.Abs(numsSorted[i] - numsSorted[i - 1]) > limit)
+                {
+                    currentGroup++;
+                }
+
+                numberToGroup[numsSorted[i]] = currentGroup;
+
+                if (!groupToList.ContainsKey(currentGroup))
+                {
+                    groupToList[currentGroup] = new LinkedList<int>();
+                }
+                groupToList[currentGroup].AddLast(numsSorted[i]);
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num = nums[i];
+                int group = numberToGroup[num];
+                nums[i] = groupToList[group].First();
+                groupToList[group].RemoveFirst();
+            }
+
+            return nums;
+        }
+    }
+}
---
+++ TestProject/LexicographicallySmallestArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode._2948.MakeLexicographicallySmallestArrayBySwappingElements;
+
+namespace TestProject
+{
+    public class LexicographicallySmallestArray
+    {
+        [Fact]
+        public void Test()
+        {
+            Solution solution = new();
+
+            int[] result1 = solution.LexicographicallySmallestArray([1, 5, 3, 9, 8], 2);
+            int[] result2 = solution.LexicographicallySmallestArray([1, 7, 6, 18, 2, 1], 3);
+            int[] result3 = solution.LexicographicallySmallestArray([1, 7, 28, 19, 10], 3);
+            int[] result4 = solution.LexicographicallySmallestArray([1000000000], 1);
+
+            Assert.Equal([1, 3, 5, 8, 9], result1);
+            Assert.Equal([1, 6, 7, 18, 1, 2], result2);
+            Assert.Equal([1, 7, 28, 19, 10], result3);
+            Assert.Equal([1000000000], result4);
+        }
+    }
+}
