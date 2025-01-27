136e6023ec01a46b694c401e6a0237eee181de4c
markrissinger@gmail.com <markrissinger@gmail.com>
2025-01-26 20:28:29 -0500

    daily leetcode - course schedule iv

---
+++ LeetCode/1462.CourseScheduleIV/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._1462.CourseScheduleIV
+{
+    public class Solution
+    {
+        public IList<bool> CheckIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries)
+        {
+            Dictionary <int, List<int>> adjacentList = new();
+
+            foreach (int[] prereq in prerequisites)
+            {
+                adjacentList.TryAdd(prereq[0], new());
+                adjacentList[prereq[0]].Add(prereq[1]);
+            }
+
+            List<bool> result = new();
+            for (int i = 0; i < queries.Length; i++)
+            {
+                result.Add(IsPrerequisite(adjacentList, new bool[numCourses], queries[i][0], queries[i][1]));
+            }
+
+            return result;
+        }
+
+        private bool IsPrerequisite(Dictionary<int, List<int>> adjacentList, bool[] visited, int source, int target)
+        {
+            visited[source] = true;
+
+            if (source == target)
+            {
+                return true;
+            }
+
+            bool result = false;
+            List<int> neighbors = adjacentList.ContainsKey(source) ? adjacentList[source] : new();
+            foreach (int neighbor in neighbors)
+            {
+                if (!visited[neighbor])
+                {
+                    result = result || IsPrerequisite(adjacentList, visited, neighbor, target);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TestProject/CheckIfPrerequisite.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode._1462.CourseScheduleIV;
+
+namespace TestProject
+{
+    public class CheckIfPrerequisite
+    {
+        [Fact]
+        public void Test()
+        {
+            Solution solution = new();
+
+            List<bool> result1 = solution.CheckIfPrerequisite(2, [[1, 0]], [[0, 1], [1, 0]]).ToList();
+            List<bool> result2 = solution.CheckIfPrerequisite(2, [], [[1, 0], [0, 1]]).ToList();
+            List<bool> result3 = solution.CheckIfPrerequisite(3, [[1, 2], [1, 0], [2, 0]], [[1, 0], [1, 2]]).ToList();
+
+            Assert.Equal([false, true], result1);
+            Assert.Equal([false, false], result2);
+            Assert.Equal([true, true], result3);
+        }
+    }
+}
