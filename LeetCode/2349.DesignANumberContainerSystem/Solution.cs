e920de15975745710b725a3dbfc1d9e5b3284fb2
markrissinger@gmail.com <markrissinger@gmail.com>
2025-02-07 19:37:03 -0500

    daily leetcode - design a number container system

---
+++ LeetCode/2349.DesignANumberContainerSystem/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._2349.DesignANumberContainerSystem
+{
+    public class NumberContainers
+    {
+        public NumberContainers()
+        {
+            _numberToIndices = new();
+            _indexToNumbers = new();
+        }
+
+        Dictionary<int, List<int>> _numberToIndices;
+        Dictionary<int, int> _indexToNumbers;
+
+        public void Change(int index, int number)
+        {
+            if (_indexToNumbers.ContainsKey(index))
+            {
+                int oldNumber = _indexToNumbers[index];
+                _numberToIndices[oldNumber].Remove(index);
+                if (_numberToIndices[oldNumber].Count == 0)
+                {
+                    _numberToIndices.Remove(oldNumber);
+                }
+            }
+
+            _indexToNumbers[index] = number;
+            if (!_numberToIndices.ContainsKey(number))
+            {
+                _numberToIndices[number] = new();
+            }
+            _numberToIndices[number].Add(index);
+        }
+
+        public int Find(int number)
+        {
+            if (_numberToIndices.ContainsKey(number))
+            {
+                return _numberToIndices[number].Min();
+            }
+            return -1;
+        }
+    }
+}
---
+++ TestProject/NumberContainers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode._2349.DesignANumberContainerSystem;
+
+namespace TestProject
+{
+    public class NumberContainers
+    {
+        [Fact]
+        public void Test()
+        {
+            Solution solution = new();
+
+            int[] result1 = solution.Numb(["NumberContainers", "find", "change", "change", "change", "change", "find", "change", "find"],
+                                                             [[], [10], [2, 10], [1, 10], [3, 10], [5, 10], [10], [1, 20], [10]]);
+            int[] result2 = solution.FindRedundantConnection([[1, 2], [2, 3], [3, 4], [1, 4], [1, 5]]);
+
+            Assert.Equal([null, -1, null, null, null, null, 1, null, 2], result1);
+        }
+    }
+}
